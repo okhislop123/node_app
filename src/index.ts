c92e954bef1bf5b6775b8c9fc2902fa4258b7970
Phạm Hoàng <hoangph@sunshinegroup.vn>
2023-10-20 13:57:33 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const port = process.env.PORT ?? 4000;
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
+
+app.use("/static", express.static(path.join(__dirname, "../public")));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
